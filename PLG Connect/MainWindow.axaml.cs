bfe5964d262c9531dc126a656bfa341d7c4a6ebc
Elias Fierke <elias.fierke@outlook.com>
2024-06-01 17:36:52 +0200

    Initialized projects

---
+++ PLG Connect/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PLG_Connect
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
