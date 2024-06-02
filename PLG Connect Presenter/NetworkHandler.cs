cde042f362643364e16c5662050e8d5fb1b5eba6
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-06-02 17:58:15 +0200

    feat: client logic

--- PLG Connect Presenter/NetworkHandler.cs
+++ PLG Connect Presenter/NetworkHandler.cs
@@ -3,8 +3,12 @@ using System.Threading.Tasks;
 using WatsonWebserver;
 using WatsonWebserver.Core;
 using System.Text.Json;
+using System.Text;
 using System.Net;
+using System.Net.Http;
 using System.Collections.Generic;
+using WatsonHttpMethod = WatsonWebserver.Core.HttpMethod;
+using HttpMethod = System.Net.Http.HttpMethod;
 
 namespace PLG_Connect_Network
 {
@@ -26,13 +30,13 @@ namespace PLG_Connect_Network
             };
             Webserver server = new Webserver(settings, DefaultRoute);
 
-            server.Routes.PreAuthentication.Static.Add(HttpMethod.GET, "/ping", PingRoute);
-            server.Routes.PreAuthentication.Static.Add(HttpMethod.POST, "/displayText", DisplyTextRoute);
-            server.Routes.PreAuthentication.Static.Add(HttpMethod.POST, "/toggleBlackScreen", ToggleBlackScreenRoute);
-            server.Routes.PreAuthentication.Static.Add(HttpMethod.POST, "/runCommand", RunCommandRoute);
-            server.Routes.PreAuthentication.Static.Add(HttpMethod.POST, "/openSlide", OpenSlideRoute);
-            server.Routes.PreAuthentication.Static.Add(HttpMethod.POST, "/nextSlide", NextSlideRoute);
-            server.Routes.PreAuthentication.Static.Add(HttpMethod.POST, "/previousSlide", PreviousSlideRoute);
+            server.Routes.PreAuthentication.Static.Add(WatsonHttpMethod.GET, "/ping", PingRoute);
+            server.Routes.PreAuthentication.Static.Add(WatsonHttpMethod.POST, "/displayText", DisplyTextRoute);
+            server.Routes.PreAuthentication.Static.Add(WatsonHttpMethod.POST, "/toggleBlackScreen", ToggleBlackScreenRoute);
+            server.Routes.PreAuthentication.Static.Add(WatsonHttpMethod.POST, "/runCommand", RunCommandRoute);
+            server.Routes.PreAuthentication.Static.Add(WatsonHttpMethod.POST, "/openSlide", OpenSlideRoute);
+            server.Routes.PreAuthentication.Static.Add(WatsonHttpMethod.POST, "/nextSlide", NextSlideRoute);
+            server.Routes.PreAuthentication.Static.Add(WatsonHttpMethod.POST, "/previousSlide", PreviousSlideRoute);
 
             server.StartAsync();
         }
@@ -152,7 +156,67 @@ namespace PLG_Connect_Network
         }
     }
 
-    public class DisplayTextMessage
+    public class Client
+    {
+        public required string ServerAddress { get; set; }
+        static readonly HttpClient client = new HttpClient();
+
+        private async Task sendPostRequest<T>(string path, T message)
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(message);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(ServerAddress + path, content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return;
+            }
+            catch (HttpRequestException e)
+            {
+                return;
+            }
+        }
+
+        public async Task DisplayText(string text)
+        {
+            var message = new DisplayTextMessage { Text = text };
+            await sendPostRequest<DisplayTextMessage>("/displayText", message);
+        }
+
+        public async Task ToggleBlackScreen()
+        {
+            var message = new object();
+            await sendPostRequest<object>("/toggleBlackScreen", message);
+        }
+
+        public async Task RunCommand(string command)
+        {
+            var message = new RunCommandMessage { Command = command };
+            await sendPostRequest<RunCommandMessage>("/runCommand", message);
+        }
+
+        public async Task OpenSlide(string slidePath)
+        {
+            var message = new OpenSlideMessage { SlidePath = slidePath };
+            await sendPostRequest<OpenSlideMessage>("/openSlide", message);
+        }
+
+        public async Task NextSlide()
+        {
+            var message = new object();
+            await sendPostRequest<object>("/nextSlide", message);
+        }
+
+        public async Task PreviousSlide()
+        {
+            var message = new object();
+            await sendPostRequest<object>("/previousSlide", message);
+        }
+    }
+
+    class DisplayTextMessage
     {
         public required string Text { get; set; }
     }
