b9f5ebcfad6abace1bd5991c401e424baeb49ba5
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-06-02 16:33:59 +0200

    ref: json deserialize abstraction

--- PLG Connect Presenter/NetworkHandler.cs
+++ PLG Connect Presenter/NetworkHandler.cs
@@ -29,6 +29,17 @@ namespace PLG_Connect_Presenter
             server.StartAsync();
         }
 
+        static T ExtractObject<T>(HttpContextBase ctx)
+        {
+            var result = JsonSerializer.Deserialize<T>(ctx.Request.DataAsString);
+            if (result == null)
+            {
+                throw new Exception("Invalid JSON");
+            }
+
+            return result;
+        }
+
         static async Task DefaultRoute(HttpContextBase ctx)
         {
             await ctx.Response.Send("Hello from the default route!");
@@ -42,18 +53,11 @@ namespace PLG_Connect_Presenter
         // General purpose routes
         static async Task DisplyTextRoute(HttpContextBase ctx)
         {
-            string message;
+            DisplayTextMessage result;
 
             try
             {
-                var result = JsonSerializer.Deserialize<DisplayTextMessage>(ctx.Request.DataAsString);
-                if (result == null)
-                {
-                    ctx.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                    await ctx.Response.Send("ERROR: Invalid JSON");
-                    return;
-                }
-                message = result.Text;
+                result = ExtractObject<DisplayTextMessage>(ctx);
             }
             catch (Exception e)
             {
@@ -71,12 +75,38 @@ namespace PLG_Connect_Presenter
         }
         static async Task RunCommandRoute(HttpContextBase ctx)
         {
+            RunCommandMessage result;
+
+            try
+            {
+                result = ExtractObject<RunCommandMessage>(ctx);
+            }
+            catch (Exception e)
+            {
+                ctx.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await ctx.Response.Send($"ERROR: {e.Message}");
+                return;
+            }
+
             await ctx.Response.Send("");
         }
 
         // Slide Routes
         static async Task OpenSlideRoute(HttpContextBase ctx)
         {
+            OpenSlideMessage result;
+
+            try
+            {
+                result = ExtractObject<OpenSlideMessage>(ctx);
+            }
+            catch (Exception e)
+            {
+                ctx.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await ctx.Response.Send($"ERROR: {e.Message}");
+                return;
+            }
+
             await ctx.Response.Send("");
         }
 
@@ -94,5 +124,15 @@ namespace PLG_Connect_Presenter
     public class DisplayTextMessage
     {
         public required string Text { get; set; }
+    }
+
+    public class RunCommandMessage
+    {
+        public required string Command { get; set; }
+    }
+
+    public class OpenSlideMessage
+    {
+        public required string SlidePath { get; set; }
     }
 }
