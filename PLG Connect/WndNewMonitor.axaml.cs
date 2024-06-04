ed6e7686f1da97c2b43bc8001f8ebfb824ab3964
Elias Fierke <elias.fierke@outlook.com>
2024-06-04 20:22:47 +0200

    added new-monitor logic

--- PLG Connect/WndNewMonitor.axaml.cs
+++ PLG Connect/WndNewMonitor.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace PLG_Connect;
@@ -9,5 +10,69 @@ public partial class WndNewMonitor : Window
     public WndNewMonitor()
     {
         InitializeComponent();
+    }
+
+
+    public bool start_n = true;
+    public bool start_ip = true;
+    private void TextBox_GotFocus(object sender, Avalonia.Input.GotFocusEventArgs e)
+    {
+        TextBox? tb = (sender as TextBox);
+        if (tb == null)
+        {
+            return;
+        }
+        if (start_n && tb.Text == "Enter name...")
+        {
+            tb.Text = "";
+            start_n = false;
+        }
+    }
+
+    private void TextBox_GotFocus_1(object sender, Avalonia.Input.GotFocusEventArgs e)
+    {
+        TextBox? tb = (sender as TextBox);
+        if (tb == null)
+        {
+            return;
+        }
+        if (start_ip && tb.Text == "Enter IP...")
+        {
+            tb.Text = "";
+            start_ip = false;
+        }
+    }
+    bool finished = false;
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (!start_n && !start_ip)
+        {
+            MainWindow.new_mon_temp_ip = TbIP.Text;
+            MainWindow.new_mon_temp_name = TbName.Text;
+            MainWindow.new_mon_canceled = false;
+            finished = true;
+        }
+
+
+        Close();
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void Window_Closing(object sender, Avalonia.Controls.WindowClosingEventArgs e)
+    {
+        if (!finished)
+        {
+            MainWindow.new_mon_canceled = true;
+        }
+    }
+
+    private void TextBox_TextChanged(object sender, Avalonia.Controls.TextChangedEventArgs e)
+    {
+
     }
 }
