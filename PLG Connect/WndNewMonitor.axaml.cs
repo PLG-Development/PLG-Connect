1034c3e3bf689f3c8798a529e64591f1e0c07601
Elias Fierke <elias.fierke@outlook.com>
2024-06-04 20:03:08 +0200

    initialized new monitor window

---
+++ PLG Connect/WndNewMonitor.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PLG_Connect;
+
+public partial class WndNewMonitor : Window
+{
+    public WndNewMonitor()
+    {
+        InitializeComponent();
+    }
+}
