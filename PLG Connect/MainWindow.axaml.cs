0e54f1bd86c0b45f8feead8bd64addfde8ddf5d5
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-06-05 10:26:18 +0200

    feat: config storing

--- PLG Connect/MainWindow.axaml.cs
+++ PLG Connect/MainWindow.axaml.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
 using Avalonia.Controls;
+using System.Text.Json;
+using System.IO;
+using PLG_Connect_Network;
+using System;
 
 
 namespace PLG_Connect;
@@ -12,6 +16,13 @@ partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        ConfigPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "PLG-Connect",
+            "config.json"
+        );
+        LoadConfig();
     }
 
     // New Monitor static variables just let them here
@@ -20,13 +31,38 @@ partial class MainWindow : Window
     public static string new_mon_temp_mac = "";
     public static bool new_mon_canceled = false;
 
+    private string ConfigPath;
+
     private void SaveConfig()
     {
-
+        string json = JsonSerializer.Serialize(Displays);
+        File.WriteAllText(ConfigPath, json);
     }
 
     private void LoadConfig()
     {
+        if (!File.Exists(ConfigPath))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(ConfigPath)!);
+            File.WriteAllText(ConfigPath, "[]");
+        }
 
+        string json = File.ReadAllText(ConfigPath);
+        Displays = JsonSerializer.Deserialize<List<Display>>(json)!;
     }
+}
+
+class Display : ClientConnection
+{
+    public required DisplaySettings Settings;
+
+    public Display(DisplaySettings settings) : base(settings.IPAddress, settings.MacAddress) { }
+}
+
+
+struct DisplaySettings
+{
+    public string Name;
+    public string IPAddress;
+    public string MacAddress;
 }
