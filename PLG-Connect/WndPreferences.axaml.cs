ccca8560c9562a60dd7818a5b59b73ead03f1ac2
Elias Fierke <elias.fierke@outlook.com>
2024-08-06 08:48:22 +0200

    Initialized preferences window (executable)

---
+++ PLG-Connect/WndPreferences.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PLG_Connect;
+
+public partial class WndPreferences : Window
+{
+    public WndPreferences()
+    {
+        InitializeComponent();
+    }
+}
