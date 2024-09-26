6408c271f474ee7b6020ccf3850f1d95dc6b9360
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-09-27 01:19:20 +0200

    chore: nix dev shell support (#9)
    * feat: basic nix flakes support
    
    * ref: remove unneeded nix code

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    lib = nixpkgs.lib;
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = with pkgs; [
+        dotnet-sdk_8
+      ];
+
+      LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+        stdenv.cc.cc
+        fontconfig
+        xorg.libX11
+        xorg.libICE
+        xorg.libSM
+      ];
+      NIX_LD = "${pkgs.stdenv.cc}/bin/ld";
+    };
+  };
+}
