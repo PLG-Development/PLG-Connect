ab679f848fa28e686ae17dc64053fb82e23c3e6e
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-09-27 01:14:52 +0200

    ref: remove unneeded nix code

--- flake.nix
+++ flake.nix
@@ -13,35 +13,16 @@
     lib = nixpkgs.lib;
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = with pkgs; [
+      packages = with pkgs; [
         dotnet-sdk_8
-        dotnet-runtime_8
-        avalonia
-        dotnetPackages.Nuget
-        mono
-        skia
-
-        xorg.libX11
-        xorg.libX11.dev
-        xorg.libICE
-        xorg.libSM
-        xorg.libXinerama
-        fontconfig
-        libGL
-        libxkbcommon
-        glib
-        freetype
       ];
 
       LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
         stdenv.cc.cc
-        skia
-        fontconfig
         fontconfig
         xorg.libX11
         xorg.libICE
         xorg.libSM
-        dotnet-sdk_8
       ];
       NIX_LD = "${pkgs.stdenv.cc}/bin/ld";
     };
