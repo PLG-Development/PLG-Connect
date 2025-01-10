8b71fe5c0787a9c3c7976f12a06f729b548bf226
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-01-10 17:01:24 +0100

    deps: fix flake

--- flake.nix
+++ flake.nix
@@ -13,7 +13,13 @@
     devShells."x86_64-linux".default = pkgs.mkShell {
       packages = with pkgs; [
         dotnet-sdk_9
+        dotnet-runtime_9
+        dotnet-aspnetcore_9
         wmctrl
+
+        omnisharp-roslyn
+        dotnetPackages.Nuget
+        # avalonia
       ];
 
       LD_LIBRARY_PATH = with pkgs;
@@ -23,8 +29,12 @@
           xorg.libX11
           xorg.libICE
           xorg.libSM
+          zlib
+          openssl
         ];
       NIX_LD = "${pkgs.stdenv.cc}/bin/ld";
+
+      DOTNET_ROOT = "${pkgs.dotnet-sdk_9}";
     };
   };
 }
