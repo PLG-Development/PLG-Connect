a71a26a7fdc8d9998e9cbf6901c1274cafd3d297
Elias Fierke <elias.fierke@outlook.com>
2025-05-01 13:44:37 +0200

    [fix:] MessageBox could not appear after window was closed

--- PLG-Connect/MessageBox.axaml.cs
+++ PLG-Connect/MessageBox.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System;
 using System.Threading.Tasks;
 
 namespace PLG_Connect;
@@ -14,48 +16,54 @@ public partial class MessageBox : Window
 
     public static Task<MessageBoxResult> Show(Window parent, string text, string title, MessageBoxButton buttons = MessageBoxButton.Ok)
     {
-        var msgbox = new MessageBox()
-        {
-            Title = title
-        };
-        msgbox.FindControl<TextBlock>("Text").Text = text;
-        var buttonPanel = msgbox.FindControl<StackPanel>("Buttons");
-
-        var res = MessageBoxResult.Ok;
-
-        void AddButton(string caption, MessageBoxResult r, bool def = false)
-        {
-            var btn = new Button { Content = caption };
-            btn.Click += (_, __) =>
+        try{
+            var msgbox = new MessageBox()
             {
-                res = r;
-                msgbox.Close();
+                Title = title
             };
-            buttonPanel.Children.Add(btn);
-            if (def)
-                res = r;
-        }
+            msgbox.FindControl<TextBlock>("Text").Text = text;
+            var buttonPanel = msgbox.FindControl<StackPanel>("Buttons");
 
-        if (buttons == MessageBoxButton.Ok || buttons == MessageBoxButton.OkCancel)
-            AddButton("Ok", MessageBoxResult.Ok, true);
-        if (buttons == MessageBoxButton.YesNo || buttons == MessageBoxButton.YesNoCancel)
+            var res = MessageBoxResult.Ok;
+
+            void AddButton(string caption, MessageBoxResult r, bool def = false)
+            {
+                var btn = new Button { Content = caption };
+                btn.Click += (_, __) =>
+                {
+                    res = r;
+                    msgbox.Close();
+                };
+                buttonPanel.Children.Add(btn);
+                if (def)
+                    res = r;
+            }
+
+            if (buttons == MessageBoxButton.Ok || buttons == MessageBoxButton.OkCancel)
+                AddButton("Ok", MessageBoxResult.Ok, true);
+            if (buttons == MessageBoxButton.YesNo || buttons == MessageBoxButton.YesNoCancel)
+            {
+                AddButton("Yes", MessageBoxResult.Yes);
+                AddButton("No", MessageBoxResult.No, true);
+            }
+
+            if (buttons == MessageBoxButton.OkCancel || buttons == MessageBoxButton.YesNoCancel)
+                AddButton("Cancel", MessageBoxResult.Cancel, true);
+
+
+
+            var tcs = new TaskCompletionSource<MessageBoxResult>();
+            msgbox.Closed += delegate { tcs.TrySetResult(res); };
+            if (parent != null)
+                msgbox.ShowDialog(parent);
+            else msgbox.Show();
+
+            return tcs.Task;
+        } catch (Exception ex)
         {
-            AddButton("Yes", MessageBoxResult.Yes);
-            AddButton("No", MessageBoxResult.No, true);
+            Logger.Log("Error while showing messagebox: " + ex.Message);
+            return Task.FromResult(MessageBoxResult.Error);
         }
-
-        if (buttons == MessageBoxButton.OkCancel || buttons == MessageBoxButton.YesNoCancel)
-            AddButton("Cancel", MessageBoxResult.Cancel, true);
-
-
-
-        var tcs = new TaskCompletionSource<MessageBoxResult>();
-        msgbox.Closed += delegate { tcs.TrySetResult(res); };
-        if (parent != null)
-            msgbox.ShowDialog(parent);
-        else msgbox.Show();
-
-        return tcs.Task;
     }
 }
 public enum MessageBoxButton
@@ -71,5 +79,6 @@ public enum MessageBoxResult
     Ok,
     Cancel,
     Yes,
-    No
+    No,
+    Error
 }
